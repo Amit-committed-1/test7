e79c2b2cb4fc16fcd7cc1678839acbb8d48e22c6
Amit-committed-1 <AmitBhanj76@gmail.com>
2025-03-13 18:44:31 +0530

    Update Test7Application.java
    "Some Fields are added"

--- src/main/java/com/test7/Test7Application.java
+++ src/main/java/com/test7/Test7Application.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test7Application {
 
 	public static void main(String[] args) {
+                String name = "mike";
+	        int x = 100;
+	        boolean isPresent = false;
+	        int y = 200;
+	        char x1 = 'a';
+	        double z = 10.3;
+	        float m = 10.3f;
+	        char ch = 'Z';
+	        boolean isSubscribed = false;
 		SpringApplication.run(Test7Application.class, args);
 	}
 
